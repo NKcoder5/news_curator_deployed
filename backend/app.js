af4583182c0df073b797678eb336039d31973499
NkCoder5 <nandhakumar4115@gmail.com>
2025-08-31 15:15:09 +0530

    server port rectified properly

--- backend/app.js
+++ backend/app.js
@@ -68,5 +68,5 @@ app.get('/health', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running at ${PORT}`);
+  console.log(`✅ Server is running at http://localhost:${PORT}`);
 });

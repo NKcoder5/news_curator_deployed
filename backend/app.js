6aa304786140dbd540fc951269838df0a0b2002e
NkCoder5 <nandhakumar4115@gmail.com>
2025-08-31 13:35:47 +0530

    cors problem solved

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'https://news-curator-deployed-1.onrender.com', // Actual deployed frontend
+  'https://pure-press.netlify.app',
 ];
 app.use(cors({
   origin: function (origin, callback) {

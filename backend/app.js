beab5964ff725e79b289ada22f7055f7d006c8ba
NkCoder5 <nandhakumar4115@gmail.com>
2025-08-31 14:56:52 +0530

    server port rectified

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const connectDB = require('./utils/db'); // New
 require('./models');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 'https://news-curator-deployed.onrender.com';
 
 // Middleware
 const allowedOrigins = [
